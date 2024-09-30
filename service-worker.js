fe703974e3aa8b54bb4e38b120a9cac375b97670
harry-577 <137449187+harry-577@users.noreply.github.com>
2024-09-30 17:27:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,6 +9,7 @@ const urlsToCache = [
   "/input.css",
   "/output.css",
   "/manifest.json",
+  "/service-worker.js",
   "/Images4Estate/Favicon 32x32.jpg",
   "/Images4Estate/house 360 192x192.png",
   "/Images4Estate/house 360 512x512.png",
